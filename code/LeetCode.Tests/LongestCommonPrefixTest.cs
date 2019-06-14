64baa4f0bbf3d06da34eaa7f1a2d32219dcf7e2d
colin.lu <colinlu.win@gmail.com>
2019-06-14 18:27:58 +0800

    add Questions/LongestCommonPrefix.cs

---
+++ code/LeetCode.Tests/LongestCommonPrefixTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LeetCode.Questions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Tests
+{
+    [TestClass]
+    public class LongestCommonPrefixTest
+    {
+        [TestMethod]
+        public void GetLongestCommonPrefix_flower_flow_flight()
+        {
+            var srv = new LongestCommonPrefix();
+            string s = srv.GetLongestCommonPrefix(new string[] { "flower", "flow", "flight" });
+
+            Assert.AreEqual("fl", s);
+        }
+
+        [TestMethod]
+        public void GetLongestCommonPrefix_dog_racecar_car()
+        {
+            var srv = new LongestCommonPrefix();
+            string s = srv.GetLongestCommonPrefix(new string[] { "dog", "racecar", "car" });
+
+            Assert.AreEqual("", s);
+        }
+    }
+}
---
+++ code/LeetCode/Questions/LongestCommonPrefix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode.Questions
+{
+    public class LongestCommonPrefix
+    {
+        public string GetLongestCommonPrefix(string[] strs)
+        {
+            string result = "";
+
+            int idx = 0;
+            bool isOver = false;
+            while (true)
+            {
+                char current = strs[0][idx];
+                foreach (var s in strs)
+                {
+                    if (s[0] != current)
+                        isOver = true;
+
+                    if (idx >= s.Length)
+                        isOver = true;
+                }
+
+                result += current;
+
+                if (isOver)
+                    break;
+
+                ++idx;
+            }
+
+            return result;
+        }
+    }
+}
